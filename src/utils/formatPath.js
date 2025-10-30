f79edde8e5cb48520979d821d3b1cdd110b0e0ef
Lyall <58636255+Lyall-A@users.noreply.github.com>
2025-10-30 10:39:53 +0000

    normalize download path when formatting

--- src/utils/formatPath.js
+++ src/utils/formatPath.js
@@ -4,7 +4,11 @@ const formatString = require('./formatString');
 
 function formatPath(unformattedPath, obj) {
     const { root } = path.parse(unformattedPath);
-    return `${root}${unformattedPath.replace(root, '').split(path.sep).map(i => formatString(i, obj).replace(/\/|\\|\?|\*|\:|\||\"|\<|\>/g, '')).join(path.sep)}`;
+    return `${root}${path.normalize(unformattedPath)
+        .replace(root, '')
+        .split(path.sep)
+        .map(i => formatString(i, obj).replace(/\/|\\|\?|\*|\:|\||\"|\<|\>/g, ''))
+        .join(path.sep)}`;
 };
 
 module.exports = formatPath;
